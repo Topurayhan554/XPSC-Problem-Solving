407d1fecf9dc94325424ded61d8b6ee65c53fef4
Topu Rayhan <topurayhantipu@gmail.com>
2024-10-27 23:09:52 +0600

    Update B_Find_Multiple.cpp

--- DAY-1/B_Find_Multiple.cpp
+++ DAY-1/B_Find_Multiple.cpp
@@ -1,19 +1,19 @@
-#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
-
-int main() {
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
     int A, B, C;
     cin >> A >> B >> C;
-
-    // Iterate from A to B to find a multiple of C
-    for (int i = A; i <= B; i++) {
-        if (i % C == 0) {
-            cout << i << endl; // Print the first multiple of C found
-            return 0; // Exit after finding the first valid number
+    for (int i = A; i <= B; i++)
+    {
+        if (i % C == 0)
+        {
+            cout << i << endl;
+            return 0;
         }
     }
-
-    // If no multiple is found, print -1
     cout << -1 << endl;
     return 0;
 }
